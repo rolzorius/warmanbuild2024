e7b68cb8f7a0007478cfbec00765f7d5cad80551
rolzorius <132336163+rolzorius@users.noreply.github.com>
2024-05-18 16:44:54 +1000

    Update Warman_Code_Base.ino
    Updated with new Stepper library function definitions and calls. Please call these as they're shown in CASE 2 section.

--- Warman_Code_Base.ino
+++ Warman_Code_Base.ino
@@ -1,6 +1,6 @@
 // Library inclusions
 #include <Servo.h>         // Servo Library
-#include <AccelStepper.h>  // Stepper Library
+#include <AFMotor.h> // Motor
 
 // Pins
 #define SERVOPIN 10
@@ -11,9 +11,11 @@ Servo myservo;
 #define ENA 9
 #define dirPin_1 5    // Direction pin STEPPER 1
 #define stepPin_1 6   // Step pin STEPPER 1
-#define dirPin_2 X    // Direction pin STEPPER 2 - need to put pin variable in
-#define stepPin_2 Y   // Step pin STEPPER 2 - need to put pin variable in
+#define dirPin_2 X    // Direction pin STEPPER 2
+#define stepPin_2 Y   // Step pin STEPPER 2
 #define motorInterfaceType 1
+#define enc1 Z  // encoder for extend
+#define enc2 XZ // encoder for drive
 
 // Global Variables
 int pos = 0;
@@ -27,18 +29,21 @@ int fromLow = 0;      // Lowest value seen by potentiometer.
 int fromHigh = 1023;  // Highest value seen by potentiometer.
 int toLow = 50;       // Lowest value where the motor starts to spin. May need adjusting
 int toHigh = 255;     // Highest value you can send to motor controller.
+int enc1_count = digitalRead(enc1); // Encoder1 count >>> 20 = 1 full rotation
 
 unsigned long startMillis;           //
 unsigned long currentMillis;         // Count milliseconds once a timer starts.
 const unsigned long period = 10000;  // This is 10 seconds in milliseconds.
 unsigned long delayedstart;
 
-#define stepsPerRevolution = 2048;
-AccelStepper stepper1 = AccelStepper(motorInterfaceType, stepPin_1, dirPin_1);
-AccelStepper stepper2 = AccelStepper(motorInterfaceType, stepPin_2, dirPin_2);
+#define stepsPerRevolution_1 = 200 // as per motor datasheet.
+#define stepsPerRevolution_2 = 200 // as per motor datasheet.
+AF_Stepper stepper1(200, 1);    // Stepper for the turret rotation. This defines stepper1(steps/rev, channel#). i.e. 200steps/rev, channel 1.
+AF_Stepper stepper2(200, 2);    // Stepper for the arm elevation.
 
 // Define booleans
 bool button_released = true;
+
 
 // Initial setup code - RUN ONCE
 void setup() {
@@ -49,8 +54,9 @@ void setup() {
   digitalRead(BUTTON);
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-  stepper.setMaxSpeed(500);
-  stepper.setAcceleration(500)
+  stepper1.setSpeed(20);  // Use this to change stepper1 speed.
+  stepper2.setSpeed(20);  // Use this to change stepp2 speed. REMEMBER; THE HIGHER THE SPEED THE LOWER THE TORQUE!
+  pinMode(enc1, INPUT);
 }
 
 // Movement functions
@@ -74,17 +80,34 @@ void breakStop() {  // Quickly stops, regardless of direction.
   digitalWrite(IN2, LOW);
 }
 
+void extend_1() {
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  for (enc_1_count <= 20){
+    analogWrite(XXX, 155);
+  }
+  
+
+}
+
+void rotate_CCW() {
+  s
+  digitalWrite(dirPin_1, LOW); // set direction CCW
+  digitalWrite(stepPin_1, HIGH); // Step pin to high, causes rotation
+
+}
+
 
 // Main loop
 
 void loop() {
 
   switch (task) {
-
-    case 1:  // Task 1 - Idling, waiting for button push.
+    case 1:
+      // Task 1 - Idling, waiting for button push.
       while (digitalRead(BUTTON) == 1){
       Serial.print("Waiting for instructions.");
-      if (digitalRead(BUTTON) == 0) {    // if button pressed, switch to task 2. 
+      if (digitalRead(BUTTON) == 0) {
         Serial.print("Switching to task 2!");
         task = 2;
       }
@@ -92,7 +115,14 @@ void loop() {
   
 
   case 2:
-    // Task 2 - 
+    // Task 2 - Turn turret so arm is in on intercept course with first ball. Raise arm to relevant height and extend.
+    stepper1.step(100, FORWARD, SINGLE);
+    //stepper2.step(100, FORWARD, SINGLE);
+    
+    
+    
+    
+    
     while (digitalRead(BUTTON) == 1) {  // while button is NOT pressed,
       Serial.print("Executing task 2.")
         digitalWrite(LED, HIGH);
